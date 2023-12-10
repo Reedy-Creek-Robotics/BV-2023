c388fc9ef6b8fcbb70aa1bdb17352004cfeab298
Stephan J Roorda <sjroorda@gmail.com>
2023-12-10 14:08:53 -0500

    Working BLUE element detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BVColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BVColorTest.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -13,14 +16,49 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Autonomous
+@TeleOp
 public class BVColorTest extends LinearOpMode{
+
+    //Global vars
+
+    //Color constants
+
+    //RGB
+
+    final Scalar BLUE = new Scalar(0, 0, 255);
+    final Scalar RED = new Scalar(255, 0, 0);
+    final Scalar PURPLE = new Scalar(255, 0, 255);
+    final Scalar GREEN = new Scalar(0, 255, 0);
+
+    //HSV
+
+    final Scalar LOW_BLUE = new Scalar(100, 100, 100);
+    final Scalar HIGH_BLUE = new Scalar(130, 255, 255);
+
+    List<MatOfPoint> contours = new ArrayList<>();
+
+    //Stores the converted RGB to HSV Mat
+    Mat hsvMat = new Mat();
+    //Stores a 'bitmap' of the values in range of color
+    Mat inRangeMat = new Mat();
+    //Designs how the morph var is stored
+    Mat kernel = Mat.ones(7, 7, CvType.CV_8U);
+    //Stores the morphed Mat which has most sound removed
+    Mat morph = new Mat();
+    //Stores the information of a contours' image topology, unused
+    Mat hierarchy = new Mat();
+    //Output; stores the output
+    Mat output = new Mat();
+
+    //--------------------------------------------------------
 
     //Webcam initialization
     OpenCvWebcam webcam;
@@ -28,33 +66,10 @@ public class BVColorTest extends LinearOpMode{
     //Pipeline initialization
     OpenCvPipeline Processor = new OpenCvPipeline() {
 
-        /*
-         * Some color constants
-         */
-        final Scalar BLUE = new Scalar(0, 0, 255);
-        final Scalar RED = new Scalar(255, 0, 0);
-        final Scalar PURPLE = new Scalar(255, 0, 255);
-        final Scalar GREEN = new Scalar(0, 255, 0);
-        final Scalar LOW_RED = new Scalar(130, 0, 0);
-        final Scalar HIGH_RED = new Scalar(180, 0, 0);
-
         @Override
         public Mat processFrame(Mat input) {
 
             //Variables for processFrame()
-
-            //Stores the converted RGB to HSV Mat
-            Mat hsvMat = new Mat();
-            //Stores a 'bitmap' of the values in range of color
-            Mat inRangeMat = new Mat();
-            //Designs how the morph var is stored
-            Mat kernel = Mat.ones(7, 7, CvType.CV_8U);
-            //Stores the morphed Mat which has most sound removed
-            Mat morph = new Mat();
-            //Stores the information of a contours' image topology, unused
-            Mat hierarchy = new Mat();
-            //Output; stores the output
-            Mat output = new Mat();
 
             //-----------------------------------------------------
 
@@ -62,7 +77,7 @@ public class BVColorTest extends LinearOpMode{
             Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
 
             //Creates a bitmap based on if the color is within the two scalar values
-            Core.inRange(hsvMat, LOW_RED, RED, inRangeMat);
+            Core.inRange(hsvMat, LOW_BLUE, HIGH_BLUE, inRangeMat);
 
             //Removes excess sound to create contours easily
             Imgproc.morphologyEx(inRangeMat, morph, Imgproc.MORPH_CLOSE, kernel);
@@ -72,26 +87,18 @@ public class BVColorTest extends LinearOpMode{
             List<MatOfPoint> contours = new ArrayList<>();
             Imgproc.findContours(morph, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
-            for (int i = 0; i < contours.size(); i++) {
-                Imgproc.drawContours(output, contours, i, RED, -1, 2);
-            }
+            BVColorTest.this.contours = contours;
 
-            //Returns output to webcam
-            return output;
+            //Returns input to webcam
+            return input;
         }
     };
 
-    public BVColorTest () {
+    @Override
+    public void runOpMode() throws InterruptedException {
 
-        /*Upon initializing the webcam with the OpenCvWebcam class via hardwareMap,
-        driver station returns an NullPointerException error (attempted to access
-        a null object).*/
-        webcam = hardwareMap.get(OpenCvWebcam.class, "Camera");
-
-        // We set the viewport policy to optimized view so the preview doesn't appear 90 deg
-        // out when the RC activity is in portrait. We do our actual image processing assuming
-        // landscape orientation, though.
-        webcam.setViewportRenderingPolicy(OpenCvWebcam.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Camera"), cameraMonitorViewId);
 
         webcam.setPipeline(Processor);
 
@@ -101,7 +108,7 @@ public class BVColorTest extends LinearOpMode{
             public void onOpened()
             {
                 Log.d("OPENCV", "WEBCAM STARTED STREAMING");
-                webcam.startStreaming(800,600);
+                webcam.startStreaming(800,600, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
@@ -113,16 +120,23 @@ public class BVColorTest extends LinearOpMode{
             }
         });
 
-    }
-
-    @Override
-    public void runOpMode() throws InterruptedException {
+        while (opModeInInit()) {for (int i = 0; i < contours.size(); i++) {
+            Imgproc.drawContours(output, contours, i, RED, 1, 1);
+            }
+        }
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-        }
+            telemetry.addData("Contours Detected", contours.size());
 
+            List<MatOfPoint> contours = this.contours;
+            for (int i = 0; i < contours.size(); i++) {
+                telemetry.addData("Contour Points: ", contours);
+            }
+
+            telemetry.update();
+        }
     }
 }
