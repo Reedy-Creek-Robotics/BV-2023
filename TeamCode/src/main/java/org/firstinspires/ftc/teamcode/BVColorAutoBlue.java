238a212263dfb2273a10213b67b74e55936b8e9f
Stephan J Roorda <sjroorda@gmail.com>
2023-12-16 13:56:19 -0500

    Completely merged in everything-- stuff still doesn't work...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BVColorAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BVColorAutoBlue.java
@@ -1,27 +1,82 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import static org.firstinspires.ftc.teamcode.BVColorProcessor.blueContourArea;
-import static org.firstinspires.ftc.teamcode.BVColorProcessor.blueProcessor;
-import static org.firstinspires.ftc.teamcode.BVColorProcessor.contoursBlue;
-import static org.firstinspires.ftc.teamcode.BVColorProcessor.merge;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Autonomous
 public class BVColorAutoBlue extends LinearOpMode {
 
     final Scalar RED = new Scalar(255, 0, 0);
 
+    //HSV Blue
+    final Scalar LOW_BLUE = new Scalar(100, 100, 100);
+    final Scalar HIGH_BLUE = new Scalar(130, 255, 255);
+
+    List<MatOfPoint> contoursBlue = new ArrayList<>();
+
+    //Stores the converted RGB to HSV Mat
+    Mat hsvMat = new Mat();
+    //Stores a 'bitmap' of the values in range of color
+    Mat inRangeMat = new Mat();
+    //Designs how the morph var is stored
+    Mat kernel = Mat.ones(7, 7, CvType.CV_8UC1);
+    //Stores the morphed Mat which has most sound removed
+    Mat morph = new Mat();
+    //Stores the information of a contours' image topology, unused
+    Mat hierarchy = new Mat();
+    //Output; stores the output for drawing contours if wanted
+    Mat output = new Mat();
+    //Blue contour area; stores a different contour area value depending on the iteration of the for loop it is on
+    double blueContourArea;
+
+    //--------------------------------------------------------
+
     OpenCvWebcam webcam;
+
+    OpenCvPipeline blueProcessor = new OpenCvPipeline() {
+
+        @Override
+        public Mat processFrame(Mat input) {
+
+            //Converts all color from RGB to HSV
+            Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
+
+            //Creates a bitmap based on if the color is within the two scalar values
+            Core.inRange(hsvMat, LOW_BLUE, HIGH_BLUE, inRangeMat);
+
+            //Removes excess sound to create contours easily
+            Imgproc.morphologyEx(inRangeMat, morph, Imgproc.MORPH_CLOSE, kernel);
+            Imgproc.morphologyEx(morph, morph, Imgproc.MORPH_OPEN, kernel);
+
+            //Creates a list (array) of contours based on the now morphed image
+            List<MatOfPoint> contours = new ArrayList<>();
+
+            Imgproc.findContours(morph, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            for (int i = 0; i < contoursBlue.size(); i++) {
+                blueContourArea = Imgproc.contourArea(contoursBlue.get(i));
+            }
+
+            //Returns input to webcam
+            return input;
+        }
+    };
 
         @Override
         public void runOpMode() throws InterruptedException {
@@ -44,7 +99,7 @@ public class BVColorAutoBlue extends LinearOpMode {
 
             while (opModeInInit()) {
                 for (int i = 0; i < contoursBlue.size(); i++) {
-                    Imgproc.drawContours(merge, contoursBlue, i, RED, 2, Imgproc.LINE_8);
+                    Imgproc.drawContours(morph, contoursBlue, i, RED, 2, Imgproc.LINE_8);
                 }
             }
 
@@ -65,6 +120,9 @@ public class BVColorAutoBlue extends LinearOpMode {
                         telemetry.addData("Element Detected! Area of Element:", blueContourArea);
                     }
                 }
+
+                telemetry.update();
+
             }
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BVColorAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BVColorAutoRed.java
@@ -1,29 +1,101 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.teamcode.BVColorProcessor.merge;
-import static org.firstinspires.ftc.teamcode.BVColorProcessor.redContourArea;
-import static org.firstinspires.ftc.teamcode.BVColorProcessor.redProcessor;
-import static org.firstinspires.ftc.teamcode.BVColorProcessor.contoursRed;
-
-import android.graphics.Color;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Autonomous
 public class BVColorAutoRed extends LinearOpMode {
 
+    //HSV Red
+    final Scalar LOW_RED1 = new Scalar(248, 100, 100);
+    final Scalar HIGH_RED1 = new Scalar(0, 255, 255);
+
+    final Scalar LOW_RED2 = new Scalar(0, 100, 100);
+    final Scalar HIGH_RED2 = new Scalar(5, 255, 255);
+
     final Scalar BLUE = new Scalar(0, 255, 0);
 
+    //--------------------------------------------------------
+
+    //Red Processor Vars
+    //Two vars of each since we are creating two comparisons then merging them.
+    //Refer to blue processor comments for descriptions of mats.
+
+    Mat hsvMat1 = new Mat();
+    Mat hsvMat2 = new Mat();
+
+    Mat inRangeMat1 = new Mat();
+    Mat inRangeMat2 = new Mat();
+
+    Mat morph1 = new Mat();
+    Mat morph2 = new Mat();
+
+    Mat hierarchy = new Mat();
+
+    Mat kernel = Mat.ones(7, 7, CvType.CV_8UC1);
+
+    double redContourArea;
+
+    List<MatOfPoint> contoursRed = new ArrayList<>();
+
+    Mat merge = new Mat();
+
+    //--------------------------------------------------------
+
     OpenCvWebcam webcam;
+
+    OpenCvPipeline redProcessor = new OpenCvPipeline() {
+
+        @Override
+        public Mat processFrame(Mat input) {
+
+            //Converts all color from RGB to HSV
+            Imgproc.cvtColor(input, hsvMat1, Imgproc.COLOR_RGB2HSV);
+            Imgproc.cvtColor(input, hsvMat2, Imgproc.COLOR_RGB2HSV);
+
+            //Creates a bitmap based on if the color is within the two scalar values
+            Core.inRange(hsvMat1, LOW_RED1, HIGH_RED1, inRangeMat1);
+            Core.inRange(hsvMat2, LOW_RED2, HIGH_RED2, inRangeMat2);
+
+            //Removes excess sound to create contours easily
+            Imgproc.morphologyEx(inRangeMat1, morph1, Imgproc.MORPH_CLOSE, kernel);
+            Imgproc.morphologyEx(morph1, morph1, Imgproc.MORPH_OPEN, kernel);
+
+            Imgproc.morphologyEx(inRangeMat2, morph2, Imgproc.MORPH_CLOSE, kernel);
+            Imgproc.morphologyEx(morph2, morph2, Imgproc.MORPH_OPEN, kernel);
+
+            Core.bitwise_or(morph1, morph2, merge);
+
+            //Creates a list (array) of contours based on the now morphed image
+            List<MatOfPoint> contours = new ArrayList<>();
+            //Variable for the red contour area based on the contours
+
+            Imgproc.findContours(merge, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            for (int i = 0; i < contoursRed.size(); i++) {
+                redContourArea = Imgproc.contourArea(contoursRed.get(i));
+            }
+
+            //Returns input to webcam
+            return input;
+        }
+    };
 
     public void runOpMode() throws InterruptedException {
 
@@ -35,10 +107,17 @@ public class BVColorAutoRed extends LinearOpMode {
             public void onOpened() {
                 webcam.startStreaming(800, 600, OpenCvCameraRotation.UPRIGHT);
                 webcam.setPipeline(redProcessor);
+
+                telemetry.addLine("INITIALIZATION SUCCESSFUL");
+                telemetry.update();
+
             }
 
             @Override
             public void onError(int errorCode) {
+
+                telemetry.addData("ERROR UPON INITIALIZATION:", errorCode);
+                telemetry.update();
 
             }
         });
@@ -51,17 +130,33 @@ public class BVColorAutoRed extends LinearOpMode {
 
         waitForStart();
 
+        telemetry.clearAll();
+
         while (opModeIsActive()) {
 
+            List<MatOfPoint> contours = new ArrayList<>();
+
             telemetry.addLine("Detecting RED Contours");
+
+            Imgproc.findContours(merge, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            for (int i = 0; i < contoursRed.size(); i++) {
+                redContourArea = Imgproc.contourArea(contoursRed.get(i));
+            }
+
             telemetry.addData("Contours Detected", contoursRed.size());
 
             for (int i = 0; i < contoursRed.size(); i++) {
+
                 telemetry.addData("Contour Points: ", contoursRed);
+
+                if (redContourArea > 1000 && gamepad1.x) {
+                    telemetry.addData("Element Detected! Area of Element:", redContourArea);
+                }
             }
-            if (redContourArea > 1000 && gamepad1.x) {
-                telemetry.addData("Element Detected! Area of Element:", redContourArea);
-            }
+
+            telemetry.update();
+
         }
     }
 }
